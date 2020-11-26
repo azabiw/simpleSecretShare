cc1dde514c0881e3cbdc652eb351f2a2ddd342d8
Ossi Vanhala <ozzivanhala@gmail.com>
2020-11-27 00:55:27 +0200

    add file listing and links to shared files

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const config = require("./sampleConfig.js").config;
 console.log(config);
 
 const password = config.password;
-const filePath = path.basename(config.files[0]); 
-//TODO LOG INJEKTIOLTA yms suojautuminen
+const fileNames = config.files;
 
-if (password === undefined || filePath === undefined) {
+
+//TODO LOG INJEKTIOLTA yms suojautuminen
+app.set('view engine', 'pug');
+
+
+if (password === undefined || config === undefined || config === {}) {
     console.error("ERROR | Failed loading the config object\n stopping process");
     process.exit(1);
 }
@@ -26,22 +30,45 @@ app.get("/vault/:key", (req, res) => {
 
     if (requestKey === password) {
         console.log(`SUCCESS | Succesfull request with key ${requestKey}`);
-
-
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                console.error(err);
-                res.send("File read failed");
-            }
-            console.log("INFO | reading file");
-            res.send(""+ data);
-
-          });
-
-
+        res.render("fileListing", {
+            files:config.files,
+            key:requestKey
+        })
     } else {
         console.error(`INCORRECT KEY | key ${requestKey}`);
         res.sendStatus(403);
     }
 });
+
+app.get("/vault/:key/:file", (req, res) => {
+    const requestKey = req.params.key;
+    const fileName = req.params.file.trim();
+    console.log(`ATTEMT | Request with key ${requestKey}`);
+
+    if (requestKey === password) {
+        console.log(`SUCCESS | Succesfull request with key ${requestKey}`);
+
+        if (fileNames.includes(fileName)) {
+            const filePath = path.basename(fileName); 
+
+            fs.readFile(filePath, (err, data) => {
+                if (err) {
+                    console.error(err);
+                    res.send("File read failed");
+                }
+                console.log(`INFO | sending file ${filePath}`);
+               res.send(""+data);
+              });
+        } else {
+            console.error(`ERROR | failed matching file name with given name ${fileName}`);
+            res.sendStatus(404);
+        }
+        
+    } else {
+        console.error(`INCORRECT KEY | key ${requestKey}`);
+        res.sendStatus(403);
+    }
+});
+
+
 app.listen(config.port ? config.port : 3000);
--- sampleConfig.js
+++ sampleConfig.js
@@ -1,6 +1,6 @@
 const config = {
     port:3000,
     password: "asd",
-    files: ["dummy_secret.txt"]
+    files: ["dummy_secret.txt","second_secret.txt"]
 }
 exports.config = config;
